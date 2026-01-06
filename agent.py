bd0e3cf35f4f6c8d0ec8ed2aff8f59ca4bf66d4c
PoonavKuchekar <67749288+Poonav@users.noreply.github.com>
2026-01-06 13:20:55 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -9,8 +9,8 @@ def run_travel_agent(user_input):
     # 1️⃣ Flight search (FIXED)
     flight_result = flight_search.invoke({
         "flight_input": {
-            "source": user_input["source"],
-            "destination": user_input["destination"]
+            "source": user_input["source"][0],
+            "destination": user_input["destination"][0]
         }
     })
 
